812e60a9a8cacc9df414e238a8cd157b975aa647
terrenceme <65180917+terrenceme@users.noreply.github.com>
2021-10-06 20:18:42 -0400

    Created apk

---
+++ group19_debug_apk/Customer.java
@@ -0,0 +1,12 @@
+package com.example.group19_debug_apk;
+
+public class Customer {
+    String username;String password; String firstName;String lastName;
+    public Customer(String username, String password, String firstName, String lastName){
+        this.username = username;
+        this.password = password;
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+}
---
+++ group19_debug_apk/Employee.java
@@ -0,0 +1,11 @@
+package com.example.group19_debug_apk;
+
+public class Employee {
+    private String username;private String password;private String firstName;private String lastName;
+    public Employee(String username, String password, String firstName, String lastName){
+        this.username = username;
+        this.password = password;
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+}
