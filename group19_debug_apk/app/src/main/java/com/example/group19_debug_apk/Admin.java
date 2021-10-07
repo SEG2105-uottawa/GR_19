4fd4981a4502251fea6dd3a6e6c16bf5dc48fe9e
terrenceme <65180917+terrenceme@users.noreply.github.com>
2021-10-06 20:32:45 -0400

    Updated group19_debug_apk

---
+++ group19_debug_apk/app/src/main/java/com/example/group19_debug_apk/Admin.java
@@ -0,0 +1,10 @@
+package com.example.group19_debug_apk;
+
+public class Admin {
+    String username;
+    String password;
+    public Admin(){
+        username = "admin";
+        password = "admin";
+    }
+}
--- group19_debug_apk/app/src/main/java/com/example/group19_debug_apk/Employee.java
+++ group19_debug_apk/app/src/main/java/com/example/group19_debug_apk/Employee.java
@@ -1,7 +1,7 @@
 package com.example.group19_debug_apk;
 
 public class Employee {
-    private String username;private String password;private String firstName;private String lastName;
+    String username;String password;String firstName; String lastName;
     public Employee(String username, String password, String firstName, String lastName){
         this.username = username;
         this.password = password;
